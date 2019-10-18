7405cba0aff13dfe412987736694ec54d188a818
Paul Friederichsen <floogulinc@gmail.com>
2019-10-18 14:59:53 -0500

    Add comments to EchoServer

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -9,19 +9,24 @@ import java.net.Socket;
 public class EchoServer {
     public static void main(String[] args) {
         try {
+            // Listen on port 6013
             ServerSocket socket = new ServerSocket(6013);
             
-            while (true) {
-                Socket client = socket.accept();
+            while (true) { // Loop forever so we can accept multiple clients
+                Socket client = socket.accept(); // Waits until a client connects, then accepts the connection
                 
                 InputStream input = client.getInputStream();
                 OutputStream output = client.getOutputStream(); 
                 
-                int read;
+                int read; // The byte we will be reading
+
+                // While there is still input to read
                 while ((read = input.read()) != -1)
-                    output.write(read);
+                    output.write(read); // Write the input from the client back to the client
                 
-                output.flush();
+                output.flush(); // flush the output stream
+
+                // Shutdown and close the connection to the client
                 client.shutdownOutput();
                 client.close();
             }
