4ec4dedce5ea8858089e11b7af0ff22a3f82f20a
Paul Friederichsen <floogulinc@gmail.com>
2019-10-18 14:29:25 -0500

    Implement EchoServer

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,33 @@
 package echoserver;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class EchoServer {
+    public static void main(String[] args) {
+        try {
+            ServerSocket socket = new ServerSocket(6013);
+            
+            while (true) {
+                Socket client = socket.accept();
+                
+                InputStream input = client.getInputStream();
+                OutputStream output = client.getOutputStream(); 
+                
+                int read;
+                while ((read = input.read()) != -1)
+                    output.write(read);
+                
+                output.flush();
+                client.shutdownOutput();
+                client.close();
+            }
+        } catch (IOException exception) {
+            System.err.println(exception);
+        } 
+    }
+    
 }
