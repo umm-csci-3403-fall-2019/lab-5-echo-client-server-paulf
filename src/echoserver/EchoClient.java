060c01ab9087d05d34860612ff9e20ec1701985e
Paul Friederichsen <floogulinc@gmail.com>
2019-10-18 14:59:45 -0500

    Add comments to EchoClient

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -9,20 +9,25 @@ import java.net.Socket;
 public class EchoClient {
     public static void main(String[] args) {
         try {
+            // Connect to the server (localhost if no argument provided)
             Socket socket = new Socket(args.length > 0 ? args[0] : "127.0.0.1", 6013);
             InputStream input = socket.getInputStream();
             OutputStream output = socket.getOutputStream();
             
-            int read;
+            int read; // The byte we will be reading
+
+            // While there is still input to read
             while ((read = System.in.read()) != -1) {
-                output.write(read);
-                System.out.write(input.read());
+                output.write(read); // Write the input from the stdin to output stream (the server)
+                System.out.write(input.read()); // Output to stdout the input coming from the server
             } 
             
+            // Shutdown and close the socket
             socket.shutdownOutput();
             socket.close();
             
-            System.out.flush();
+            System.out.flush(); // Flush the stdout stream
+
         } catch (ConnectException exception) {
             System.out.println("Unable to connect to the server");
             System.err.println(exception);
